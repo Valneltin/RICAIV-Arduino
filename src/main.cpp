6f5ae780fca02fc4c11f200a6dd9b95a00e5811b
Valneltin <val.laurent.vl@gmail.com>
2018-06-18 17:10:46 +0200

    Modify reception code and add UNO bord to target plateform.io

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,24 @@
 #include <Arduino.h>
 
-string ReceivedMessage;
-int IRL_data;
-int IRR_data;
-int Gyro_1_data;
-int Gyro_2_data;
-int Gyro_3_data;
+int ReceivedMessage = 0;
+
 
 void setup() {
-    Serial.begin(115200)
+    Serial.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  if (Serial.available()) // Test if the buffer is empty
+void loop()
+{
+
+  if (Serial.available() == -1) // Test if the buffer is empty
   {
-    ReceivedMessage = Serial.read(); // Read buffer
-
-    switch (ReceivedMessage) // Begin test on ReceivedMessage
-    {
-      case "IRL":
-        IRL_data = measure_IRL();
-        break;
-
-      case "IRR":
-        IRR_data = measure_IRR();
-        break;
-
-      case "Gyro_1":
-        Gyro_1_data = measure_Gyro(1);
-        break;
-
-      case "Gyro_2":
-        Gyro_2_data = measure_Gyro(2);
-        break;
-
-      case "Gyro_3":
-        Gyro_3_data = measure_Gyro(3);
-        break;
-
-      default:
-        break;
-    }
 
   }
-}
-
-
-/***** MEASUREMENT FUNCTION *****/
-
-/* Measure distance of left arm*/
-int measure_IRL() {
-
-}
-
-/* Measure distance of right arm */
-int measure_IRR() {
-
-}
-
-/* Measure the gyro */
-int measure_Gyro(i) {
-
+  else
+  {
+    ReceivedMessage = Serial.read(); // Read buffer
+    Serial.print("Message : ");
+    Serial.println(ReceivedMessage);
+  }
 }
