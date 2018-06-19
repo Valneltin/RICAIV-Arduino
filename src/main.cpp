2b6525c07846f37b482a48d887faf89cc29f7d83
Valneltin <val.laurent.vl@gmail.com>
2018-06-19 16:49:13 +0200

    Arduino can received LT, RT and JOY command

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 
 String ReceivedMessage = "";
+String LTValue = "";
+String RTValue = "";
+String ValeurH = "";
 
 void setup() {
     Serial.begin(115200); //Begin serial link
@@ -8,10 +11,36 @@ void setup() {
 
 void loop()
 {
+  ReceivedMessage = "";
+  delay(1);
 
   if (Serial.available()) // Test if the buffer is empty
   {
     ReceivedMessage = Serial.readString(); // Read buffer
-    Serial.print("Message : ["+ReceivedMessage+"]"); //Send received message to serial link
+    // Serial.print("Message : ["+ReceivedMessage+"]"); // Send received message to serial link
+
+    if (ReceivedMessage.startsWith("JOY_")) // Test if is a joystick information
+    {
+      Serial.println("JOY_OK");
+    }
+    else if (ReceivedMessage.startsWith("LT_")) // Test if is a LT command
+    {
+      Serial.println("LT_OK");
+      ReceivedMessage.replace("LT_", "");
+      LTValue = ReceivedMessage;
+      Serial.println("LT Value = ["+LTValue+"]");
+    }
+    else if (ReceivedMessage.startsWith("RT_")) // Test if is a RT command
+    {
+      Serial.println("RT_OK");
+      ReceivedMessage.replace("RT_", "");
+      RTValue = ReceivedMessage;
+      Serial.println("RT Value = ["+RTValue+"]");
+    }
+    else // This information is not acceptable
+    {
+      Serial.println("Je ne comprends rien ^^");
+    }
+    delay(10);
   }
 }
