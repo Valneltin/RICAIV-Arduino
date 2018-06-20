cb1e88542742c19f85e902bce9fde534b188e190
Valneltin <val.laurent.vl@gmail.com>
2018-06-20 19:37:02 +0200

    Add IR function

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@ String LTValue = "";
 String RTValue = "";
 String ValeurH = "";
 
+int pinPR=A0; //pin de connexion pour la photor�sistance
+
+float valeurtension;
+float valeurcm;
+
 void setup() {
     Serial.begin(115200); //Begin serial link
 }
@@ -45,18 +50,16 @@ void loop()
   }
 }
 
-void uneFonction(){
+void IR(){
+  int valeur=analogRead(pinPR); // on lit la valeur transmise par la photor�sistance
+  valeurtension = valeur*4.97/1023;
+  //valeurcm = 4.4405*pow(valeurtension,-1.385); // calcul moins pr�cis mais plus simple. observ� fiable.
+  valeurcm = -1.48*pow(valeurtension,5) + 14.233*pow(valeurtension,4) - 53.985*pow(valeurtension,3) + 100.94*pow(valeurtension,2) - 95.813*valeurtension + 41.05;
 
-}
+  //valeurcm = -1*exp(-11*pow(valeurtension,5)) + 2*exp(-8*pow(valeurtension,4)) - 1*exp(-5*pow(valeurtension,3)) + 0.0031*pow(valeurtension,2) - 0.5072*valeurtension + 40.99; //formule sens�e am�liorer encore la pr�cision, non test�e.
+  Serial.println(valeurtension); // on l'affiche
+  Serial.println(valeurcm); // on l'affiche
+  Serial.println("  ");
 
-void uneSecondeFonction(){
-
-}
-
-void uneQuatriemeFonction(){
-
-}
-
-void uneTroisiemeFonction(){
-
+  delay(300); // wait for 0.3 second
 }
