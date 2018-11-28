1b99c9438310c8d55f1c8771c8a88f23747f580b
Valneltin <val.laurent.vl@gmail.com>
2018-11-28 16:21:21 +0100

    Finalize command motor

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 /***  VARIABLES ***/
 
 String ReceivedMessage;
-char buffer [21];
+char buffer [25];
 
 int leftarmforward;
 int leftarmbackward;
@@ -30,24 +30,24 @@ int duty_cycle_RM = 0;
 // Left Schoulder
 int pwmLS = 4;
 int dirLS = 24;
-int speedforwardLS = 255;
-int speedbackwardLS = 1;
+int speedforwardLS = 1000;
+int speedbackwardLS = -1000;
 // Right Schoulder
 int pwmRS = 5;
 int dirRS = 25;
-int speedforwardRS = 255;
-int speedbackwardRS = 1;
+int speedforwardRS = 1000;
+int speedbackwardRS = -1000;
 
 // Left Arm
 int pwmLA = 6;
 int dirLA = 26;
-int speedforwardLA = 255;
-int speedbackwardLA = 1;
+int speedforwardLA = 1000;
+int speedbackwardLA = -1000;
 // Right Arm
 int pwmRA = 7;
 int dirRA = 27;
-int speedforwardRA = 255;
-int speedbackwardRA = 1;
+int speedforwardRA = 1000;
+int speedbackwardRA = -1000;
 
 
 /*___________ SET UP ___________*/
@@ -75,26 +75,19 @@ void setup() {
 
 
 /*___________ SUB FUNCTION ___________*/
-/*** Calcul the duty cycle from value of potentiometer's value ***/
-int dutyCycleCalcul(int speed){
-  int value = map(speed, 0, 200, 0, 255);
-  return value;
-}
 
-/*** Set value of speed for motors in function of duty cycle ***/
-void motor(int pinpwm, int pindir, int duty_cycle ){
-  if (duty_cycle <128) // Motor go backward
-  {
-    boolean dirvalue = LOW;           // Set direction value of motor
-    duty_cycle = 2*128-duty_cycle;    // Calculate PWM duty cycle to go backward
-    digitalWrite(pindir, dirvalue);   // Direction
-    analogWrite(pinpwm, duty_cycle);  // Speed
+/*** Set value of speed for motors in function of speed ***/
+void motor(int pinpwm, int pindir, int speed){
+  if (speed < 0){
+    digitalWrite(pindir, LOW);
+    speed = -speed;
+    speed = map(speed, 0, 1000, 0, 255);
+    analogWrite(pinpwm, speed);
   }
-  else if (duty_cycle>=128) // Motor go forward
-  {
-    boolean dirvalue = HIGH;          // Set direction value of motor
-    digitalWrite(pindir, dirvalue);   // Direction
-    analogWrite(pinpwm, duty_cycle);  // Speed
+  else if (speed >= 0){
+    digitalWrite(pindir, HIGH);
+    speed = map(speed, 0, 1000, 0, 255);
+    analogWrite(pinpwm, speed);
   }
 
 }
@@ -104,130 +97,92 @@ void motor(int pinpwm, int pindir, int duty_cycle ){
 void loop()
 {
 
-  /*for(int j=0;j<20;j++)
-  {
-    ReceivedMessage [j] = "";
-  }*/
-
   if (Serial.available()) // Test if the buffer is empty
   {
-    /*for (int i=0; i<20; i++){
-      buffer = Serial.read();
-      ReceivedMessage [i] = buffer; // Read buffer
-    }*/
-    ReceivedMessage = Serial.readStringUntil('e');
+
+    ReceivedMessage = Serial.readStringUntil('e'); // Read serial port until character "e"
 
     Serial.print("Message : [");
     Serial.print(ReceivedMessage);
     Serial.println("]");  // Send received message to serial link
 
-    ReceivedMessage.toCharArray(buffer, ReceivedMessage.length());
-
-    delay (10);
+    ReceivedMessage.toCharArray(buffer, ReceivedMessage.length()); // Convert ReceivedMessage in char array
 
     sscanf(buffer, "%d%d%d%d%d%d%d%d%d%d e", &leftarmforward, &leftarmbackward, &rightarmforward, &rightarmbackward, &leftshoulderforward, &leftshoulderbackward, &rightshoulderforward, &rightshoulderbackward, &leftmotorspeed, &rightmotorspeed);
-
-    Serial.println(leftarmforward);
-    Serial.println(leftarmbackward);
-    Serial.println(rightarmforward);
-    Serial.println(rightarmbackward);
-    Serial.println(leftshoulderforward);
-    Serial.println(leftshoulderbackward);
-    Serial.println(rightshoulderforward);
-    Serial.println(rightshoulderbackward);
-    Serial.println(leftmotorspeed);
-    Serial.println(rightmotorspeed);
 
     /*** CONTROL LEFT ARM ***/
     if (leftarmforward == 1 && leftarmbackward == 0){       // Left arm go forward
       motor(pwmLA, dirLA, speedforwardLA);
-      //Serial.print("LA : FORWARD");
     }
     else if (leftarmforward == 0 && leftarmbackward == 1){  // Left arm go backward
       motor(pwmLA, dirLA, speedbackwardLA);
-      //Serial.print("LA : BACKWARD");
     }
     else if (leftarmforward == 0 && leftarmbackward == 0){  // Left arm don't move
-      motor(pwmLA, dirLA, 127);
-      //Serial.print("LA : STOP 1");
+      motor(pwmLA, dirLA, 0);
     }
     else if (leftarmforward == 1 && leftarmbackward == 1){  // Left arm don't move because two buttons are pressed
-      motor(pwmLA, dirLA, 127);
-      //Serial.print("LA : STOP 2");
+      motor(pwmLA, dirLA, 0);
     }
 
     /*** CONTROL RIGHT ARM ***/
     if (rightarmforward == 1 && rightarmbackward == 0){       // Right arm go forward
       motor(pwmRA, dirRA, speedforwardRA);
-      //Serial.print("RA : FORWARD");
     }
     else if (rightarmforward == 0 && rightarmbackward == 1){  // Right arm go backward
       motor(pwmRA, dirRA, speedbackwardRA);
-      //Serial.print("RA : BACKWARD");
     }
     else if (rightarmforward == 0 && rightarmbackward == 0){  // RIght arm don't move
-      motor(pwmRA, dirRA, 127);
-      //Serial.print("RA : STOP 1");
+      motor(pwmRA, dirRA, 0);
     }
     else if (rightarmforward == 1 && rightarmbackward == 1){  // Right arm don't move because two buttons are pressed
-      motor(pwmRA, dirRA, 127);
-      //Serial.print("RA : STOP 2");
+      motor(pwmRA, dirRA, 0);
     }
 
     /*** CONTROL LEFT SHOULDER ***/
     if (leftshoulderforward == 1 && leftshoulderbackward == 0){       // Left shoulder go forward
       motor(pwmLS, dirLS, speedforwardLS);
-      //Serial.print("LS : FORWARD");
     }
     else if (leftshoulderforward == 0 && leftshoulderbackward == 1){  // Left shoulder go backward
       motor(pwmLS, dirLS, speedbackwardLS);
-      //Serial.print("LS : BACKWARD");
     }
     else if (leftshoulderforward == 0 && leftshoulderbackward == 0){  // Left shoulder don't move
-      motor(pwmLS, dirLS, 127);
-      //Serial.print("LS : STOP 1");
+      motor(pwmLS, dirLS, 0);
     }
     else if (leftshoulderforward == 1 && leftshoulderbackward == 1){  // Left shoulder don't move because two buttons are pressed
-      motor(pwmLS, dirLS, 127);
-      //Serial.print("LS : STOP 2");
+      motor(pwmLS, dirLS, 0);
     }
 
     /*** CONTROL RIGHT SHOULDER ***/
     if (rightshoulderforward == 1 && rightshoulderbackward == 0){       // Right shoulder go forward
       motor(pwmRS, dirRS, speedforwardRS);
-      //Serial.print("RS : FORWARD");
     }
     else if (rightshoulderforward == 0 && rightshoulderbackward == 1){  // Right shoulder go backward
       motor(pwmRS, dirRS, speedbackwardRS);
-      //Serial.print("RS : BACKWARD");
     }
     else if (rightshoulderforward == 0 && rightshoulderbackward == 0){  // Right shoulder don't move
-      motor(pwmRS, dirRS, 127);
-      //Serial.print("RS : STOP 1");
+      motor(pwmRS, dirRS, 0);
     }
     else if (rightshoulderforward == 1 && rightshoulderbackward == 1){  // Right shoulder don't move because two buttons are pressed
-      motor(pwmRS, dirRS, 127);
-      //Serial.print("RS : STOP 2");
+      motor(pwmRS, dirRS, 0);
     }
 
     /*** WHEEL MOTOR ***/
-    duty_cycle_LM = dutyCycleCalcul(leftmotorspeed);  // Calcul duty cycle for left motor
-    duty_cycle_RM = dutyCycleCalcul(rightmotorspeed);  // Calcul duty cycle for right motor
-    motor(pwmLM, dirLM, duty_cycle_LM);               // Set speed to left motor
-    motor(pwmRM, dirRM, duty_cycle_RM);               // Set speed to right motor
+    motor(pwmLM, dirLM, leftmotorspeed);  // Set speed to left motor
+    motor(pwmRM, dirRM, rightmotorspeed); // Set speed to right motor
 
   }
   else {
-    leftarmforward = 1;
-    leftarmbackward = 1;
-    rightarmbackward = 1;
-    rightarmforward = 1;
-    leftshoulderforward = 1;
-    leftshoulderbackward = 1;
-    rightshoulderforward = 1;
-    rightshoulderbackward = 1;
-    leftmotorspeed = 324;
-    rightmotorspeed = 324;
+    /*** Reset value if not receive value from serial port ***/
+    leftarmforward = 0;
+    leftarmbackward = 0;
+    rightarmbackward = 0;
+    rightarmforward = 0;
+    leftshoulderforward = 0;
+    leftshoulderbackward = 0;
+    rightshoulderforward = 0;
+    rightshoulderbackward = 0;
+    leftmotorspeed = 0;
+    rightmotorspeed = 0;
 
     ReceivedMessage = "";
 
@@ -237,17 +192,3 @@ void loop()
     }
   }
 }
-
-/*void IR(){
-  int valeur=analogRead(pinPR); // on lit la valeur transmise par la photor�sistance
-  valeurtension = valeur*4.97/1023;
-  //valeurcm = 4.4405*pow(valeurtension,-1.385); // calcul moins pr�cis mais plus simple. observ� fiable.
-  valeurcm = -1.48*pow(valeurtension,5) + 14.233*pow(valeurtension,4) - 53.985*pow(valeurtension,3) + 100.94*pow(valeurtension,2) - 95.813*valeurtension + 41.05;
-
-  //valeurcm = -1*exp(-11*pow(valeurtension,5)) + 2*exp(-8*pow(valeurtension,4)) - 1*exp(-5*pow(valeurtension,3)) + 0.0031*pow(valeurtension,2) - 0.5072*valeurtension + 40.99; //formule sens�e am�liorer encore la pr�cision, non test�e.
-  Serial.println(valeurtension); // on l'affiche
-  Serial.println(valeurcm); // on l'affiche
-  Serial.println("  ");
-
-  delay(300); // wait for 0.3 second
-}*/
